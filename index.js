9a251732a30d30d48d3a14955b58abbbf9926df3
Claudinei Bido <bido.c@live.com>
2020-06-08 11:02:44 +0200

    our first call at least

--- index.js
+++ index.js
@@ -1,16 +1,38 @@
-const API_URL = require('./config');
-const Search = require('./models/Search');
+ //import credentials
+ const {id, key} = require('./config');
+
+// const Search = require('./models/Search');
+const axios = require('axios');
 
 //1. Get the word from the user
 const args = process.argv.slice(2);
 const query = args[0];
 
 //if the user does not insert a word or ask for help..
-if((!query) || query.includes('--help')) {
-    console.log('Show help..');
-}
-else {
+if (!query || query.includes('--help')) {
+  console.log('Show help..');
+} else {
   //1. Make a new search send the query..
-  console.log(query);
+
+  const endpoint = 'entries';
+  const language_code = 'en-us';
+  const word_id = `${query}`;
+
+  const headers = {
+    headers: {
+      'app_id': id, 'app_key': key
+    },
+}
+  url =
+    'https://od-api.oxforddictionaries.com/api/v2/' +
+    endpoint +
+    '/' +
+    language_code +
+    '/' +
+    word_id.toLowerCase();
+
+  const res = axios.get(url, headers);
+  res.then(data => {console.log(data)}).catch(err => console.log(err.message))
+  
 }
 //2. Make a new API call and send the request and API_KEY
--- models/Search.js
+++ models/Search.js
@@ -1,1 +1,11 @@
 const axios = require("axios");
+
+// class Search{
+//     constructor(query){
+//         this.query = query;
+//         this.url = "https://od-api.oxforddictionaries.com/api/v2";
+//         this.appID = "b5515b4c";
+//         this.key = "5854b27706f4896411d484519beeec7e";
+//     }
+
+// }
